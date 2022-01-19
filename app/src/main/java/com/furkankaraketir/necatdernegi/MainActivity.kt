2edde2d0641be2ad2ed1d7d97dd71de62e413a8f
Furkan Karaketir <69571844+FurkanKaraketir@users.noreply.github.com>
2022-01-19 12:05:43 +0300

    Sign In And Sign Out

--- app/src/main/java/com/furkankaraketir/necatdernegi/MainActivity.kt
+++ app/src/main/java/com/furkankaraketir/necatdernegi/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.furkankaraketir.necatdernegi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            next()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        val emailEditText = findViewById<TextInputLayout>(R.id.emailTextField)
+
+
+        val passwordEditText = findViewById<TextInputLayout>(R.id.passwordTextField)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.editText?.text.toString()
+            val password = passwordEditText.editText?.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        next()
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+
+
+    }
+
+
+
+    private fun next() {
+        val intent = Intent(this, Menu::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/furkankaraketir/necatdernegi/Menu.kt
@@ -0,0 +1,43 @@
+package com.furkankaraketir.necatdernegi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Menu : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        auth = Firebase.auth
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.signOut){
+            auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+
+    }
+}
